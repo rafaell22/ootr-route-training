4e6c7f4a61a50f6ffdbbe55e96230e0ad6a8f996
rafaellfpc <rafaell@fleetprofitcenter.com>
2023-11-03 19:58:22 -0500

    Added component dependency graph and load views/components.

--- index.js
+++ index.js
@@ -1,63 +1,123 @@
 import { readdir, readFile } from 'node:fs/promises';
 
 import DirectedGraph from './build/classes/DirectedGraph.js';
+import Stack from './build/classes/Stack.js';
 
 (async () => {
     try {
         const fullDependencyGraph = new DirectedGraph();
-        const root = await readFile('./src/index.html', 'utf-8');
-        const viewDependencies = root.match(/<v-\S*/g).map(view => view.replace('<',''));
-        
-        // search for views used in index.html
-        //   if not found: error
-        //   if found, load view files.
-        
-
-        const viewFolders = await readdir('./src/views');
-        console.log(viewFolders); 
-
-        const views = {};
-        for (const folder of viewFolders) {
-            console.log(`Reading files from folder ${folder}`);
-            const files = await readdir(`./src/views/${folder}`);
-            console.log(`Found ${files.length} files`);
-        }
-
         const components = {};
         const componentFolders = await readdir('./src/components');
-        for(const folder of componentFolders) {
-            console.log(`Reading files from folder ${folder}`);
-            const files = await readdir(`./src/components/${folder}`);
-            console.log(`Found ${files.length} files`);
-            const html = await readFile(`./src/components/${folder}/template.html`, 'utf-8');
-            const js = await readFile(`./src/components/${folder}/script.js`, 'utf-8');
-            const css = await readFile(`./src/components/${folder}/style.css`, 'utf-8');
+        for(const component of componentFolders) {
+            console.log(`Reading files from folder ${component}`);
+            let html;
+            try {
+                html = await readFile(`./src/components/${component}/template.html`, 'utf-8');
+            } catch(errorGettingComponentHtml) {
+                console.error(`Missing template for component ${component}`);
+                console.error(errorGettingComponentHtml);
+                return;
+            }
 
-            components[folder] = {
+            let js;
+            try {
+                js = await readFile(`./src/components/${component}/script.js`, 'utf-8');
+            } catch(errorGettingComponentScript) {
+                console.error(`Missing script for component ${component}`);
+                console.error(errorGettingComponentScript);
+                return;
+            }
+
+            let css;
+            try {
+                css = await readFile(`./src/components/${component}/style.css`, 'utf-8');
+            } catch(errorGettingComponentStyle) {
+                console.error(`Missing style for component ${component}`);
+                console.error(errorGettingComponentStyle);
+                return;
+            }
+
+            components[component] = {
                 html: html,
                 js: js,
                 css: css,
             };
-            componentDependencyGraph.addVertex(folder);
+            fullDependencyGraph.addVertex(component);
             const dependencies = html.match(/c-\S*/);
             if(!dependencies) {
                 continue;
             }
             for(const componentName of dependencies) {
-                componentDependencyGraph.addEdge(folder, componentName);
+                fullDependencyGraph.addEdge(component, componentName);
             }
         }
-        console.log(componentDependencyGraph.adjacencyList)
-
-        console.log(componentDependencyGraph.printGraph());
-
-        if(componentDependencyGraph.isCyclic()) {
-            console.log('Ca\'t build the application because there are cyclic dependencies!');
+        if(fullDependencyGraph.isCyclic()) {
+            console.log('Ca\'t build the application because there are cyclic components dependencies!');
             return;
         }
 
-        const componentsOrder = componentDependencyGraph.topologicalSort();
-        console.log('componentsOrder: ', componentsOrder);
+        const views = {};
+        const viewFolders = await readdir('./src/views');
+        for(const view of viewFolders) {
+            console.log(`Reading files from folder ${view}`);
+            let html;
+            try {
+                html = await readFile(`./src/views/${view}/template.html`, 'utf-8');
+            } catch(errorGettingViewHtml) {
+                console.error(`Missing template for view ${view}`);
+                console.error(errorGettingViewHtml);
+                return;
+            }
+
+            let js;
+            try {
+                js = await readFile(`./src/views/${view}/script.js`, 'utf-8');
+            } catch(errorGettingViewScript) {
+                console.error(`Missing script for view ${view}`);
+                console.error(errorGettingViewScript);
+                return;
+            }
+
+            let css;
+            try {
+                css = await readFile(`./src/views/${view}/style.css`, 'utf-8');
+            } catch(errorGettingViewStyle) {
+                console.error(`Missing style for view ${view}`);
+                console.error(errorGettingViewStyle);
+                return;
+            }
+
+            views[view] = {
+                html: html,
+                js: js,
+                css: css,
+            };
+
+            fullDependencyGraph.addVertex(view);
+            const viewComponents = html.match(/c-\S*/);
+            if(!viewComponents) {
+                continue;
+            }
+                
+            for(const component of viewComponents) {
+                if(!fullDependencyGraph.has(component)) {
+                    console.error(`Missing dependency for component ${component} in view ${view}`);
+                    return;
+                }
+                fullDependencyGraph.addEdge(view, component);
+            }
+        }
+        fullDependencyGraph.printGraph();
+        const dependenciesOrder = fullDependencyGraph.topologicalSort();
+        console.log('componentsOrder: ', dependenciesOrder);
+
+        const dependencyStack = new Stack();
+        const root = await readFile('./src/index.html', 'utf-8');
+        const viewDependencies = root.match(/<v-\S*/g).map(view => view.replace('<',''));
+        for(const view of viewDependencies) {
+            dependencyStack.push(view);
+        }
+
     } catch(errorBuildingApplication) {
         console.log('error?')
         console.error(errorBuildingApplication);
